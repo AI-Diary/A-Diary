2457b862b93516e002c962295c7393251bb907e4
김혜진 <khj000217@naver.com>
2023-01-31 20:47:24 +0900

    feat:회원가입 페이지 제작

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,38 +1,65 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Button from '../Components/Button';
 import Input from '../Components/Input';
 
+const WrapTitle = styled.div`
+  width: fit-content;
+  margin: 1rem auto 2rem auto;
+  font-size: 1.5rem;
+  font-weight: bold;
+  align-items: center;
+  /* border: 1px solid black; */
+`;
 const WrapInput = styled.div`
   box-sizing: content-box;
   width: fit-content;
   height: fit-content;
   margin: 10% auto;
-  padding: 4rem;
-  font-size: 1.5rem;
-  align-items: center;
+  padding: 2rem 4rem;
   border: 1px solid grey;
   border-radius: 2rem;
   background-color: white;
 `;
+
+const TransSignin = styled.div`
+  margin-top: 0.5rem;
+  text-align: center;
+  /* border: 1px solid black; */
+  color: grey;
+
+  font-size: 0.8rem;
+  cursor: pointer;
+  &:hovers {
+    border-bottom: 1px solid grey;
+  }
+`;
+
 function Login() {
+  const navigate = useNavigate();
+  const ClickSignin = () => {
+    navigate(`/Signin`);
+  };
   return (
     <div>
       <WrapInput>
-        로그인
+        <WrapTitle>로그인</WrapTitle>
         <Input width='15rem' height='2.8rem' placeholder='아이디' />
         <Input
           width='15rem'
           height='2.8rem'
-          marignTop='0.5rem'
+          marignTop='1rem'
           placeholder='비밀번호'
         />
+
         <Button
           width='15rem'
           height='2.5rem'
-          margin='2rem 0rem'
+          margin='2rem 0rem 0rem 0rem'
           name='로그인'
         />
+        <TransSignin onClick={ClickSignin}>회원가입이 필요한가요?</TransSignin>
       </WrapInput>
     </div>
   );
--- frontend/src/Pages/Signin.jsx
+++ frontend/src/Pages/Signin.jsx
@@ -1,7 +1,74 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import Button from '../Components/Button';
+import Input from '../Components/Input';
+
+const WrapTitle = styled.div`
+  width: fit-content;
+  margin: 2rem auto;
+  font-size: 1.5rem;
+  font-weight: bold;
+  align-items: center;
+  /* border: 1px solid black; */
+`;
+const WrapInputs = styled.div`
+  box-sizing: content-box;
+  width: fit-content;
+  height: fit-content;
+  margin: 5% auto;
+  padding: 2rem 4rem;
+  border: 1px solid grey;
+  border-radius: 2rem;
+  background-color: white;
+`;
+
+const ExplainInput = styled.div`
+  /* width: 4rem; */
+  height: fit-content;
+  margin: 2rem 0rem 0.5rem 0rem;
+  /* border: 1px solid blue; */
+`;
 
 function Signin() {
-  return <div></div>;
+  const nagivate = useNavigate();
+  const TransMain = () => {
+    nagivate(`/Main`);
+  };
+  return (
+    <div>
+      <WrapInputs>
+        <WrapTitle>회원가입</WrapTitle>
+
+        <ExplainInput>이름</ExplainInput>
+        <Input width='15rem' height='2.8rem' />
+
+        <ExplainInput>아이디</ExplainInput>
+        <Input width='15rem' height='2.8rem' />
+
+        <ExplainInput>비밀번호</ExplainInput>
+        <Input width='15rem' height='2.8rem' marignTop='1rem' />
+
+        <ExplainInput>비밀번호 확인</ExplainInput>
+        <Input width='15rem' height='2.8rem' marignTop='1rem' />
+        <ExplainInput>이메일</ExplainInput>
+        <Input
+          width='15rem'
+          height='2.8rem'
+          marignTop='1rem'
+          placeholder='이메일'
+        />
+
+        <Button
+          width='15rem'
+          height='2.5rem'
+          margin='3rem 0rem 1.5rem 0rem'
+          name='가입하기'
+          onClick={TransMain}
+        />
+      </WrapInputs>
+    </div>
+  );
 }
 
 export default Signin;
