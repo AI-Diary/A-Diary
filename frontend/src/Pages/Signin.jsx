8ebed46a63369db875d01e9146902df29a9b3e4b
김혜진 <khj000217@naver.com>
2023-02-02 17:41:15 +0900

    feat:회원가입 빈칸/비번 확인 추가

--- frontend/src/Pages/Signin.jsx
+++ frontend/src/Pages/Signin.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Button from '../Components/Button';
@@ -31,9 +31,51 @@ const ExplainInput = styled.div`
 `;
 
 function Signin() {
+  const [name, setName] = useState();
+  const [id, setId] = useState();
+  const [pw1, setPw1] = useState();
+  const [pw2, setPw2] = useState();
+  const [email, setEmail] = useState();
+
   const nagivate = useNavigate();
-  const TransMain = () => {
-    nagivate(`/Main`);
+  // const TransMain = () => {
+  //   nagivate(`/Main`);
+  // };
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeId = (e) => {
+    setId(e.target.value);
+  };
+  const onChangePw1 = (e) => {
+    setPw1(e.target.value);
+  };
+  const onChangePw2 = (e) => {
+    setPw2(e.target.value);
+  };
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const CheckForms = () => {
+    console.log(
+      '이름 : ',
+      name,
+      ' 아이디 : ',
+      id,
+      ' pw1 : ',
+      pw1,
+      ' pw2 : ',
+      pw2,
+      ' email : ',
+      email
+    );
+    if (!name || !id || !pw1 || !pw2 || !email) {
+      alert('빈 칸을 모두 입력해주세요');
+    } else if (pw1 !== pw2) {
+      alert('비밀번호를 다시 입력해주세요');
+    } else {
+      nagivate(`/Main`);
+    }
   };
   return (
     <div>
@@ -41,26 +83,41 @@ function Signin() {
         <WrapTitle>회원가입</WrapTitle>
 
         <ExplainInput>이름</ExplainInput>
-        <Input width='15rem' height='2.8rem' />
+        <Input width='15rem' height='2.8rem' onChange={onChangeName} />
 
         <ExplainInput>아이디</ExplainInput>
-        <Input width='15rem' height='2.8rem' />
+        <Input width='15rem' height='2.8rem' onChange={onChangeId} />
 
         <ExplainInput>비밀번호</ExplainInput>
-        <Input width='15rem' height='2.8rem' marignTop='1rem' />
+        <Input
+          width='15rem'
+          height='2.8rem'
+          marignTop='1rem'
+          onChange={onChangePw1}
+        />
 
         <ExplainInput>비밀번호 확인</ExplainInput>
-        <Input width='15rem' height='2.8rem' marignTop='1rem' />
+        <Input
+          width='15rem'
+          height='2.8rem'
+          marignTop='1rem'
+          onChange={onChangePw2}
+        />
 
         <ExplainInput>이메일</ExplainInput>
-        <Input width='15rem' height='2.8rem' marignTop='1rem' />
+        <Input
+          width='15rem'
+          height='2.8rem'
+          marignTop='1rem'
+          onChange={onChangeEmail}
+        />
 
         <Button
           width='15rem'
           height='2.5rem'
           margin='3rem 0rem 1.5rem 0rem'
           name='가입하기'
-          onClick={TransMain}
+          onClick={CheckForms}
         />
       </WrapInputs>
     </div>
