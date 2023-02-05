f3caa2d64015d0eacfd3c048a00cee0db9da7410
김혜진 <khj000217@naver.com>
2023-02-05 22:57:14 +0900

    test:저장소 재연결 테스트

--- frontend/src/Pages/Write.jsx
+++ frontend/src/Pages/Write.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import Button from '../Components/Button';
+
+const DrawDiary = styled.div`
+  width: 30rem;
+  height: 20rem;
+  background-color: black;
+`;
 
 function Write() {
-  return <div>Write</div>;
+  return (
+    <div>
+      <DrawDiary />
+    </div>
+  );
 }
 
 export default Write;
