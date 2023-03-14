bed22efc7bac79c69525994bbc447ab659142eea
김혜진 <khj000217@naver.com>
2023-03-14 16:30:20 +0900

    feat:디자인 수정 및 sns공유 이미지 버튼 추가

--- frontend/src/Pages/Write.jsx
+++ frontend/src/Pages/Write.jsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
+import Menu from '../Components/Menu';
 import Button from '../Components/Button';
+import moment from 'moment';
 import Sunny from '../Images/sunny_default.png';
 import Cloudy from '../Images/cloudy_default.png';
 import Rain from '../Images/rain_default.png';
 import Snow from '../Images/snow_default.png';
 import Wind from '../Images/wind_default.png';
+import Share from '../Images/share_default.png';
 import SunnyGrey from '../Images/sunny_grey.png';
 import CloudyGrey from '../Images/cloudy_grey.png';
 import RainGrey from '../Images/rain_grey.png';
 import SnowGrey from '../Images/snow_grey.png';
 import WindGrey from '../Images/wind_grey.png';
+import ShareGrey from '../Images/share_grey.png';
 
 const Wrap = styled.div`
   /* position: absolute; */
@@ -27,19 +31,11 @@ const Wrap = styled.div`
   font-family: 'NanumGothic';
 `;
 
-const WrapButtons = styled.div`
-  /* border: 1px solid black; */
-  /* margin-left: 1rem; */
-  width: fit-content;
-  height: fit-content;
-  float: right;
-`;
-
 const WrapDiary = styled.div`
   box-sizing: border-box;
   width: fit-content;
   height: fit-content;
-  margin: 7rem auto 5rem auto;
+  margin: 3rem auto 5rem auto;
   padding: 3rem 7rem 5rem 7rem;
   border-radius: 1.5rem;
   background-color: rgba(256, 256, 256, 0.8);
@@ -53,13 +49,13 @@ const WrapTop = styled.div`
   padding: 1.1rem;
   margin-bottom: 1rem;
   border-radius: 1.2rem;
-  background-color: rgba(256, 256, 256, 0.7);
+  background-color: rgba(256, 256, 256, 0.8);
   border: 1.8px solid grey;
 `;
 
 const DateForm = styled.div`
   width: fit-content;
-  word-spacing: 0.5rem;
+  word-spacing: 0.8rem;
   font-size: 1.3rem;
   margin-left: 1.5rem;
 `;
@@ -68,7 +64,9 @@ const WrapWeather = styled.div`
   width: fit-content;
   height: fit-content;
   float: right;
-  margin-left: 4rem;
+  margin-left: 2.5rem;
+  margin-right: 1.5rem;
+  margin-top: -0.15rem;
 `;
 
 const WeatherLabel = styled.label`
@@ -101,6 +99,21 @@ const Weather = styled.input`
 
 const WeatherImage = styled.img``;
 
+const ShareButton = styled.div`
+  position: absolute;
+  /* left: 40rem; */
+  display: inline-block right;
+  width: 1.8rem;
+  height: 1.8rem;
+  /* border: 2px solid black; */
+  background-image: url(${Share});
+  &:hover {
+    background-image: url(${ShareGrey});
+  }
+  &:after {
+    background-image: url(${ShareGrey});
+  }
+`;
 const DrawDiary = styled.div`
   width: 40rem;
   height: 25rem;
@@ -141,29 +154,38 @@ const WriteDiary = styled.textarea`
 const WrapKeywordButton = styled.div`
   width: fit-content;
   height: fit-content;
-  margin: 1rem auto 0rem auto;
+  margin: 2rem auto 0rem auto;
+  /* border: 2px solid black; */
 `;
 function Write() {
-  // const time = new Date();
-  // console.log('--------------- write ---------------');
-
   const { state } = useLocation();
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
 
+  // 날씨 저장
+  const [weather, setWeather] = useState();
+  // 일기글 저장
+  const [write, setWrite] = useState('');
+
   const [ctx, setCtx] = useState();
   const [isDrawing, setIsDrawing] = useState(false);
 
+  const week = ['일', '월', '화', '수', '목', '금', '토'];
+  let date = '';
+  let year = '';
+  let month = '';
+  let day = '';
+  let dayOfWeek = '';
+
   useEffect(() => {
     const canvas = canvasRef.current;
-    // canvas.width = window.innerWidth;
-    // canvas.height = window.innerHeight * 0.5;
     canvas.width = 643;
     canvas.height = 403;
 
     const context = canvas.getContext('2d');
     context.strokeStyle = 'black';
-    context.lineWidth = 2.5;
+    // context.strokeStyle = 'transparent';
+    context.lineWidth = 2;
     contextRef.current = context;
 
     setCtx(contextRef.current);
@@ -189,12 +211,6 @@ function Write() {
       }
     }
   };
-  console.log('ctx : ', ctx);
-
-  // 날씨 저장
-  const [weather, setWeather] = useState();
-  // 일기글 저장
-  const [write, setWrite] = useState('');
 
   const onChangeText = (e) => {
     setWrite(e.target.value);
@@ -203,38 +219,29 @@ function Write() {
     setWeather(e.target.value);
   };
 
-  const week = ['일', '월', '화', '수', '목', '금', '토'];
-
-  const date = state.split('-');
-  const year = date[0];
-  const month = date[1];
-  const day = date[2];
-
-  const dayOfWeek = week[new Date(date).getDay()];
+  if (state === null) {
+    // 오늘의 일기 작성 버튼 눌렀을 때
+    const today = new Date();
+    const momentDate = moment(today).format().slice(0, 10);
+    date = momentDate.split('-');
+    year = date[0];
+    month = date[1];
+    day = date[2];
+    dayOfWeek = week[new Date(date).getDay()];
+  } else {
+    // 원하는 날짜 버튼 눌렀을 때
+    date = state.split('-');
+    year = date[0];
+    month = date[1];
+    day = date[2];
+    dayOfWeek = week[new Date(date).getDay()];
+  }
 
   return (
     <div>
       <Wrap>
-        <WrapButtons>
-          <Button
-            width='5rem'
-            height='2.5rem'
-            name='일기 저장'
-            color='white'
-            margin='2rem 1rem 0rem 0rem'
-            border='2px solid white'
-            backgroundColor='transparent;'
-          />
-          <Button
-            width='5rem'
-            height='2.5rem'
-            name='SNS 공유'
-            color='white'
-            margin='2rem 4rem 0rem 1rem'
-            border='2px solid white'
-            backgroundColor='transparent;'
-          />
-        </WrapButtons>
+        <Menu />
+
         <WrapDiary>
           <WrapTop>
             <DateForm>
@@ -309,6 +316,7 @@ function Write() {
           </WrapTop>
 
           <DrawDiary>
+            <ShareButton />
             <canvas
               ref={canvasRef}
               onMouseDown={startDrawing}
@@ -317,23 +325,35 @@ function Write() {
               onMouseLeave={finishDrawing}
             ></canvas>
           </DrawDiary>
-
+          {/* <Button width='3rem' height='2.5rem' name='지우개' /> */}
           <WriteDiary onChange={onChangeText} />
 
           <WrapKeywordButton>
             <Button
-              width='8rem'
+              width='5rem'
               height='2.5rem'
               name='키워드 추출'
-              // color='rgba(138, 80, 255, 0.5)'
-              // border='2px solid rgba(138, 80, 255, 0.5)'
-              color='grey'
-              border='2px solid grey'
+              color='rgba(138, 80, 255, 0.6)'
+              border='2px solid rgba(138, 80, 255, 0.6)'
+              // color='grey'
+              // border='2px solid grey'
               backgroundColor='transparent;'
+              hoverBackgroundColor='rgba(256, 256, 256, 0.4)'
+            />
+            <Button
+              width='5rem'
+              height='2.5rem'
+              name='일기 저장'
+              margin='0rem 0rem 0rem 2rem'
+              color='rgba(138, 80, 255, 0.6)'
+              border='2px solid rgba(138, 80, 255, 0.6)'
+              // color='grey'
+              // border='2px solid grey'
+              backgroundColor='transparent;'
+              hoverBackgroundColor='rgba(256, 256, 256, 0.4)'
             />
           </WrapKeywordButton>
         </WrapDiary>
-        <div>{weather}</div>
       </Wrap>
     </div>
   );
