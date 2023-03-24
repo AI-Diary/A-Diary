fad02efb6f36bbdd107225741b3decd29d72e63a
김혜진 <khj000217@naver.com>
2023-03-25 00:35:26 +0900

    feat:통계페이지 ui 완성

--- frontend/src/Pages/Statistics.jsx
+++ frontend/src/Pages/Statistics.jsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useState, PureComponent } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Menu from '../Components/Menu';
+import Button from '../Components/Button';
 import {
   // Radar Chart 사용할 때 필요
   Radar,
@@ -80,7 +82,8 @@ const Wrap = styled.div`
   width: 100vw;
   height: fit-content;
   min-width: 60rem;
-  border: 0.1px solid transparent;
+  /* border: 0.1px solid transparent; */
+  overflow-x: hidden;
   /* padding-bottom: 4rem; */
   background: linear-gradient(
     to bottom,
@@ -89,66 +92,110 @@ const Wrap = styled.div`
   );
 `;
 
+const WrapContent = styled.div`
+  width: 40rem;
+  height: 27rem;
+  margin: 6rem auto;
+`;
+
+const Title = styled.div`
+  /* border: 2px solid black; */
+  width: fit-content;
+  height: fit-content;
+  /* font-family: 'LogoFont'; */
+  font-family: 'NanumGothic';
+  font-size: 2rem;
+  color: white;
+`;
+
 const WrapChart = styled.div`
   box-sizing: border-box;
   display: flex;
   width: 40rem;
   height: 25rem;
-  background-color: rgba(256, 256, 256, 0.8);
-  border-radius: 4rem;
-  margin: 4rem auto 2rem auto;
-  padding: 2rem;
+  background-color: rgba(256, 256, 256, 0.85);
+  border-radius: 3rem;
+  margin: 1rem auto;
+  padding: 2.3rem 2rem 2rem 2rem;
 `;
 
 function Statics() {
+  const navigate = new useNavigate();
+  const NavigateToStatistics = () => {
+    navigate(`/MyPage`);
+  };
   return (
     <div>
       <Wrap>
         <Menu />
-        <WrapChart>
-          <ResponsiveContainer width='100%' height='100%'>
-            <RadarChart cx='50%' cy='50%' outerRadius='90%' data={EMOTIONS}>
-              <PolarGrid />
-              <PolarAngleAxis
-                dataKey='emotion'
+        <Button
+          // width='5rem'
+          // height='2.3rem'
+          width='7rem'
+          height='2.7rem'
+          name='일기 확인'
+          color='white'
+          margin='1.5rem 3.5rem 1.5rem 0rem'
+          border='2px solid white'
+          borderRadius='10rem'
+          backgroundColor='transparent;'
+          hoverBackgroundColor='rgba(256,256,256)'
+          hoverColor='rgba(51, 153, 255)'
+          // hoverBorder='2px solid rgba(51, 153, 255)'
+          onClick={NavigateToStatistics}
+          float='right'
+        />
+        <WrapContent>
+          <Title>감정 통계</Title>
+          <WrapChart>
+            <ResponsiveContainer width='100%' height='100%'>
+              <RadarChart cx='50%' cy='50%' outerRadius='90%' data={EMOTIONS}>
+                <PolarGrid />
+                <PolarAngleAxis
+                  dataKey='emotion'
+                  margin={{
+                    top: 20,
+                  }}
+                />
+                <PolarRadiusAxis angle={30} />
+                <Radar
+                  name='emotion'
+                  dataKey='value'
+                  stroke='rgba(51, 153, 255, 0.8)'
+                  fill='rgba(51, 153, 255, 0.8)'
+                  // stroke='#8884d8'
+                  // fill='#8884d8'
+                  fillOpacity={0.6}
+                />
+              </RadarChart>
+            </ResponsiveContainer>
+          </WrapChart>
+        </WrapContent>
+
+        <WrapContent>
+          <Title>요일 통계</Title>
+          <WrapChart>
+            <ResponsiveContainer width='100%' height='100%'>
+              <BarChart
+                width={500}
+                height={300}
+                data={DATES}
                 margin={{
-                  top: 20,
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
                 }}
-              />
-              <PolarRadiusAxis angle={30} />
-              <Radar
-                name='emotion'
-                dataKey='value'
-                stroke='rgba(51, 153, 255, 0.8)'
-                fill='rgba(51, 153, 255, 0.8)'
-                // stroke='#8884d8'
-                // fill='#8884d8'
-                fillOpacity={0.6}
-              />
-            </RadarChart>
-          </ResponsiveContainer>
-        </WrapChart>
-        <WrapChart>
-          <ResponsiveContainer width='100%' height='100%'>
-            <BarChart
-              width={500}
-              height={300}
-              data={DATES}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray='3 3' />
-              <XAxis dataKey='date' />
-              <YAxis />
-              <Tooltip />
-              <Bar dataKey='count' stroke='#8884d8e3' fill='#8884d8a0' />
-            </BarChart>
-          </ResponsiveContainer>
-        </WrapChart>
+              >
+                <CartesianGrid strokeDasharray='3 3' />
+                <XAxis dataKey='date' />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey='count' stroke='#8884d8e3' fill='#8884d8a0' />
+              </BarChart>
+            </ResponsiveContainer>
+          </WrapChart>
+        </WrapContent>
       </Wrap>
     </div>
   );
