37cc420c0a380b6a1f99be047f2fb011c4d444c0
jaejinaa <wowls79@tukorea.ac.kr>
2023-04-06 18:59:29 +0900

    ai server & web server 분리

---
+++ backend/AIserver/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from WordControl.Word import Word
+
+app = Flask(__name__)
+
+
+@app.route('/keyword')
+def get_keyword():
+    # text=request.form['text']
+    text="나는 너무 행복해 말이 너무 많아 너는 그렇게 생각하니 나는 아무것도 하고 싶지 않아 그냥 쿠키가 너무나도 먹고싶어 쿠키는 맛있어 그냥 바나나를 먹을까"
+    # jpype.attachThreadToJVM()
+    # print(numpy)
+    # okt=Okt()
+        #    okt=Okt()
+    # list1=okt.nouns(text)
+    
+    list2=[]
+    word=Word(text)
+    list1=word.list_word()
+    
+    return list1
+
+    
+if __name__=='__main__':
+    app.run('0.0.0.0',port=5001,debug=True)
--- backend/WebServer/app.py
+++ backend/WebServer/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flaskext.mysql import MySQL
-from WordControl.Word import Word
+
 
 mysql = MySQL()
 app = Flask(__name__)
@@ -50,20 +50,10 @@ def login():
     pw = request.form['pw']
     msg = "id: %s, pw: %s" %id %pw
     return msg
-@app.route('/keyword')
-def get_keyword():
-    # text=request.form['text']
-    text="나는 너무 행복해 말이 너무 많아 너는 그렇게 생각하니 나는 아무것도 하고 싶지 않아 그냥 쿠키가 너무나도 먹고싶어 쿠키는 맛있어 그냥 바나나를 먹을까"
-    # jpype.attachThreadToJVM()
-    # print(numpy)
-    # okt=Okt()
-        #    okt=Okt()
-    # list1=okt.nouns(text)
+
+
+
+
     
-    list2=[]
-    word=Word(text)
-    list1=word.list_word()
-    
-    return list1    
 if __name__=='__main__':
     app.run('0.0.0.0',port=5000,debug=True)
