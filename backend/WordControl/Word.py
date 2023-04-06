fcdb6e2abc80145e21ccc699dbaa6fa853b422e9
jaejinaa <wowls79@tukorea.ac.kr>
2023-04-06 18:38:46 +0900

    konlpy 수정

--- backend/WordControl/Word.py
+++ backend/WordControl/Word.py
@@ -1,5 +1,5 @@
 from konlpy.tag import Okt
-
+from .WordInspection import WordInspectoin
 
 class Word(object):
     
@@ -11,17 +11,25 @@ class Word(object):
         
         okt=Okt()
         list1=okt.nouns(self._list_word)
-        
-        return list1
+        list1=list(set(list1))
+
+        # print(list1)
+        return str(list1)
+    
+    def Word_inspection(self,list1):
+        Wordinspenct=WordInspectoin(list1)    
+        word_list=Wordinspenct.Word_Inspection()
+        # 영어
+        return word_list
         
 if __name__=='__main__':
     text="나는 너무 강아지랑 있으니까 너무 행복해"
     print(type(text))
     word=Word(text)
     words=word.list_word()
-    print(word.list_word)    
+    # print(word.list_word)    
     # word.list_word()
     # okt=Okt()
     
     # print(type(okt.nouns(text)))
-    print(words)
+    # print(words)
