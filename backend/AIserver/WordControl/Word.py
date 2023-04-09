bcd043cfbae1644828670683f4417d9543e37ebc
jaejinaa <wowls79@tukorea.ac.kr>
2023-04-09 15:08:53 +0900

    papgo적용

--- backend/AIserver/WordControl/Word.py
+++ backend/AIserver/WordControl/Word.py
@@ -1,5 +1,6 @@
 from konlpy.tag import Okt
 from .WordInspection import WordInspectoin
+from .papago import Papago
 
 class Word(object):
     
@@ -14,19 +15,44 @@ class Word(object):
         list1=list(set(list1))
 
         # print(list1)
-        return str(list1)
+        return (list1)
     
     def Word_inspection(self,list1):
         Wordinspenct=WordInspectoin(list1)    
         word_list=Wordinspenct.Word_Inspection()
         # 영어
         return word_list
+    
+    def Word_Translation(self,list1):
+        
+        list2=[]
+        for i in list1:
+           
+            papa=Papago(i)   
+             
+            list2.append(papa.papa())
+        
+        return list2
+             
+                
         
 if __name__=='__main__':
+
+        
     text="나는 너무 강아지랑 있으니까 너무 행복해"
-    print(type(text))
+    # print(type(text))
     word=Word(text)
-    words=word.list_word()
+    words1=word.list_word()
+    
+    # words1=list(words1)
+    print((words1))    
+    # papa=Papago(text)
+    # print(papa.papa())
+    # words=['나','강아지']
+    # if (words==words1):
+        # print("true")
+    words=word.Word_Translation(words1)
+    print((words))
     # print(word.list_word)    
     # word.list_word()
     # okt=Okt()
--- backend/AIserver/app.py
+++ backend/AIserver/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from WordControl.Word import Word
+from WordControl.papago import Papago
 
 app = Flask(__name__)
 
@@ -8,18 +9,18 @@ app = Flask(__name__)
 def get_keyword():
     # text=request.form['text']
     text="나는 너무 행복해 말이 너무 많아 너는 그렇게 생각하니 나는 아무것도 하고 싶지 않아 그냥 쿠키가 너무나도 먹고싶어 쿠키는 맛있어 그냥 바나나를 먹을까"
-    # jpype.attachThreadToJVM()
-    # print(numpy)
     # okt=Okt()
-        #    okt=Okt()
     # list1=okt.nouns(text)
     
-    list2=[]
     word=Word(text)
     list1=word.list_word()
-    
-    return list1
+    # papago=Papago(list1)
+    # list1=papago.papa()
+    list1=word.Word_Translation(list1)
+    return str(list1)
 
+    
+    
     
 if __name__=='__main__':
     app.run('0.0.0.0',port=5001,debug=True)
