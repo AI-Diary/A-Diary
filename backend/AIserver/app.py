090b475b0ff3aee5d6a7a2d6ff487631ee4582f6
jaejinaa <wowls79@tukorea.ac.kr>
2023-04-11 22:19:09 +0900

    app.py 수정

--- backend/AIserver/app.py
+++ backend/AIserver/app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from WordControl.Word import Word
-from WordControl.papago import Papago
 from Kobert.Kobert import Kobert_predict
 
 app = Flask(__name__)
 
 
-@app.route('/keyword')
+@app.route('/keyword',methods=['POST'])
 def get_keyword():
-    # text=request.form['text']
-    text="나는 너무 행복해 말이 너무 많아 너는 그렇게 생각하니 나는 아무것도 하고 싶지 않아 그냥 쿠키가 너무나도 먹고싶어 쿠키는 맛있어 그냥 바나나를 먹을까"
+    text=request.form['text']
+    # text="나는 너무 행복해 말이 너무 많아 너는 그렇게 생각하니 나는 아무것도 하고 싶지 않아 그냥 쿠키가 너무나도 먹고싶어 쿠키는 맛있어 그냥 바나나를 먹을까"
     # okt=Okt()
     # list1=okt.nouns(text)
+    
+    kp=Kobert_predict()
+    emotion=kp.predict(text)
+    # render_template('Write.jsx',emotion=emotion[-1])
     
     word=Word(text)
     Ko_word=word.list_word()
@@ -21,10 +24,7 @@ def get_keyword():
     
     re_En_word=word.Word_inspection(En_word)
     
-    kp=Kobert_predict()
-    kp.predict(text)
-    
-    return str(Ko_word)
+    return render_template('Write.jsx',keyword=(str(Ko_word)),emotion=emotion[-1])
 
 @app.route('/drawpic')
 def get_drawpic():
