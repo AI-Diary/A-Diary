cc22ef072d12d01d573cf4ff41e491adab93d321
JiyoonYY <118877127+JiyoonYY@users.noreply.github.com>
2023-04-26 02:11:56 +0900

    feat:일기 저장 수정, 마이페이지 추가

--- backend/WebServer/app.py
+++ backend/WebServer/app.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 from flask_cors import CORS
 from flaskext.mysql import MySQL
-#import hashlib
-
+from datetime import datetime
 
 mysql = MySQL()
 app = Flask(__name__)
 CORS(app)
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-app.config['MYSQL_DATABASE_DB'] = 'adiary'
-app.config['MYSQL_DATABASE_HOST'] = 'a-diary.cqtwhnyeqcos.ap-southeast-2.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'passwd'
+app.config['MYSQL_DATABASE_DB'] = 'a-diary'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.secret_key = "ABCDEFG"
 mysql.init_app(app)
 
@@ -30,17 +29,11 @@ def signin():
         id = params['id']
         pw = params['pw']
         email = params['email']
-        #pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+        
         print('username : ',username, ' id : ',id, ' pw : ', pw, ' email : ', email)
 
-        # # 아이디 중복 확인
         conn = mysql.connect()
         cursor = conn.cursor()
-        # cursor.execute("SELECT * FROM user WHERE id = %s", (id))
-        # result = cursor.fetchall()
-
-        # if result:
-        #     return "fail"
         
         # 사용자 정보 추가
         cursor.execute("INSERT INTO user(username, id, pw, email) VALUES (%s, %s, %s, %s)", (username, id, pw, email))
@@ -49,7 +42,6 @@ def signin():
         cursor.close()
         conn.close()
     
-    # 페이지 return 수정해야함
     return "success"
 
 # 아이디 중복 확인
@@ -93,10 +85,10 @@ def login():
             is_pw_correct = user_info[3]
             print("passwd check:", is_pw_correct)
 
-            # 페이지 return 수정해야함
+            
             return "success"
         else:
-            # 페이지 return 수정해야함
+            
             return "fail" 
 
 @app.route('/main')
@@ -107,22 +99,66 @@ def write():
 @app.route('/write', methods=['POST'])
 def save_diary():
     params = request.get_json()
-    date = params['date']
-    weather = params['weather']
-    title = params['title']
-    diary = params['diary']
-    img = params['jpgUrl']
-    mood = params['emotion']
-    
-    print('weather: ',weather, 'title: ', title, 'diary: ', diary, 'img: ',img)
-
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO user_diary(title, mood, weather, diary, date, img) VALUES (%s, %s, %s, %s, %s, %s)", (title, mood, weather, diary, date, img))
-    conn.commit()
+    error = None
+    if request.method == 'POST':
+        userid = params['userid']
+        date = params['date']
+        weather = params['weather']
+        title = params['title']
+        diary = params['diary']
+        img = params['jpgUrl']
+        mood = params['emotion']
         
-    cursor.close()
-    conn.close()
+        print('userid:', userid, 'weather: ',weather, 'title: ', title, 'diary: ', diary, 'img: ',img)
+
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO user_diary(userid, title, mood, weather, diary, date, img) VALUES (%s, %s, %s, %s, %s, %s, %s)", (userid, title, mood, weather, diary, date, img))
+        conn.commit()
+            
+        cursor.close()
+        conn.close()
+
+    return "success"
+
+@app.route('/mypage', methods = ['POST'])
+def my_page():
+    if request.method == 'POST':
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        id = session['login'][0]
+
+        cursor.execute("SELECT * FROM user_diary WHERE userid = %s", (id))
+        rows = cursor.fetchall()
+
+        result = []
+        for row in rows:
+            diarynum = row[1]
+            title = row[2]
+            mood = row[3]
+            weather = row[4]
+            diary = row[5]
+            date_str = row[6]
+            createat_str = row[7]
+            img = row[8]
+
+            date = datetime.strptime(date_str, "%Y-%m-%d").date()
+            createat = datetime.strptime(createat_str, "%Y-%m-%d %H:%M:%S")
+
+            result.append({
+                "diarynum": diarynum,
+                "title": title,
+                "mood": mood,
+                "weather": weather,
+                "diary": diary,
+                "date": date,
+                "createat": createat,
+                "img": img
+            })
+
+        print('mypage', result)
+
+    return jsonify(result)
 
 # 로그아웃 기능
 @app.route('/logout')
